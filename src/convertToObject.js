ca25a74507c1a17a16b3778bffa2f0041cd8d1ec
Ivan <kovalevivan420@gmail.com>
2025-07-06 14:20:50 +0200

    solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,33 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const result = {};
+  const cleanString = (str) => str.replace(/[\t\n]/g, '');
+  const newString = cleanString(sourceString);
+  let array = [];
+  const words = newString.split(';');
+
+  if (sourceString === '' || undefined) {
+    return result;
+  }
+
+  for (const i of words) {
+    array = i.split(': ');
+
+    if (array.length > 1) {
+      array[1] = array[1].trim();
+      array[1] = array[1].split(' ');
+      array[1] = array[1].join(' ');
+
+      if (array[1].includes(',')) {
+        array[1] = array[1].split(',');
+        array[1] = array[1].join(',\n');
+      }
+      result[array[0].trim()] = array[1];
+    }
+  }
+
+  return result;
 }
 
 module.exports = convertToObject;
